3916cfb41edb6ec57defbd6ecb88be205f338f71
laileon <leon@igola.com>
2018-09-07 16:43:47 +0800

    animation set

--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/FlyWithDogeAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/FlyWithDogeAnimationActivity.kt
@@ -1,6 +1,27 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
+
 class FlyWithDogeAnimationActivity : BaseAnimationActivity() {
-  override fun onStartAnimation() {
-  }
+    override fun onStartAnimation() {
+        val positionAnimator = ValueAnimator.ofFloat(0f, -screenHeight)
+        positionAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            rocket.translationY = value
+            doge.translationY = value
+        }
+
+        val rotationAnimator = ValueAnimator.ofFloat(0f, 360f)
+        rotationAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            rocket.rotation = value
+            doge.rotation = value
+        }
+
+        val animatorSet = AnimatorSet()
+        animatorSet.play(positionAnimator).with(rotationAnimator)
+        animatorSet.duration = BaseAnimationActivity.DEFAULT_ANIMATION_DURATION
+        animatorSet.start()
+    }
 }
--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/LaunchAndSpinViewPropertyAnimatorAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/LaunchAndSpinViewPropertyAnimatorAnimationActivity.kt
@@ -1,6 +1,12 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
 class LaunchAndSpinViewPropertyAnimatorAnimationActivity : BaseAnimationActivity() {
-  override fun onStartAnimation() {
-  }
+//    ViewPropertyAnimator may provide better performance for multiple simultaneous animations
+    override fun onStartAnimation() {
+        rocket.animate()
+                .translationY(-screenHeight)
+                .rotation(720f)
+                .setDuration(BaseAnimationActivity.DEFAULT_ANIMATION_DURATION)
+                .start()
+    }
 }
