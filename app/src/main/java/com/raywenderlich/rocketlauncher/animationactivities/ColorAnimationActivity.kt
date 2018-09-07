bb95569eb4e20bcdf1b722f2e51cf7b556e07fc8
laileon <leon@igola.com>
2018-09-07 15:29:35 +0800

    color animation

--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/ColorAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/ColorAnimationActivity.kt
@@ -1,6 +1,25 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.support.v4.content.ContextCompat
+import com.raywenderlich.rocketlauncher.R
+
 class ColorAnimationActivity : BaseAnimationActivity() {
-  override fun onStartAnimation() {
-  }
+    override fun onStartAnimation() {
+        val objectAnimator = ObjectAnimator.ofObject(
+                frameLayout,
+                "backgroundColor",
+                ArgbEvaluator(),
+                ContextCompat.getColor(this, R.color.background_from),
+                ContextCompat.getColor(this, R.color.background_to)
+        )
+
+        objectAnimator.repeatCount = ValueAnimator.INFINITE
+        objectAnimator.repeatMode = ValueAnimator.REVERSE
+
+        objectAnimator.duration = BaseAnimationActivity.DEFAULT_ANIMATION_DURATION
+        objectAnimator.start()
+    }
 }
--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/LaunchRocketObjectAnimatorAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/LaunchRocketObjectAnimatorAnimationActivity.kt
@@ -1,6 +1,23 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
+import android.animation.ObjectAnimator
+
 class LaunchRocketObjectAnimatorAnimationActivity : BaseAnimationActivity() {
-  override fun onStartAnimation() {
-  }
+//  ObjectAnimator, a subclass of ValueAnimator. If you only need to animate a single property of a single object,
+//    ObjectAnimator may just be your new best friend.
+
+    override fun onStartAnimation() {
+        val objectAnimator = ObjectAnimator.ofFloat(rocket, "translationY", 0f, -screenHeight)
+        objectAnimator.duration = BaseAnimationActivity.Companion.DEFAULT_ANIMATION_DURATION
+        objectAnimator.start()
+    }
 }
+
+
+//Here’s what you’re doing:
+//Creating an instance of ObjectAnimator (like you did with ValueAnimator) except that the former takes two more parameters:
+//rocket is the object to animate
+//The object must have a property corresponding to the name of the property you wish to change, which in this example is “translationY”. You’re able to do this because rocket is an object of class View, which, in its base Java class, has an accessible setter with setTranslationY().
+//You set the duration for the animation and start it.
+
+//There’s a limitation to ObjectAnimator — it can’t animate two objects simultaneously. To work around it, you create two instances of ObjectAnimator.
