0c694003ab1da70b150a116ef2b41ccb73f98780
laileon <leon@igola.com>
2018-09-07 16:55:38 +0800

    xml animation set

--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/FlyThereAndBackAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/FlyThereAndBackAnimationActivity.kt
@@ -1,6 +1,21 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
+import android.animation.ValueAnimator
+
 class FlyThereAndBackAnimationActivity : BaseAnimationActivity() {
-  override fun onStartAnimation() {
-  }
+    override fun onStartAnimation() {
+        val animator = ValueAnimator.ofFloat(0f, -screenHeight)
+
+        animator.addUpdateListener {
+            val value = it.animatedValue as Float
+            rocket.translationY = value
+            doge.translationY = value
+        }
+
+        animator.repeatMode = ValueAnimator.REVERSE
+        animator.repeatCount = ValueAnimator.INFINITE
+
+        animator.duration = 500L
+        animator.start()
+    }
 }
--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/WithListenerAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/WithListenerAnimationActivity.kt
@@ -1,6 +1,40 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
+import android.animation.Animator
+import android.animation.ValueAnimator
+import android.widget.Toast
+
 class WithListenerAnimationActivity : BaseAnimationActivity() {
-  override fun onStartAnimation() {
-  }
+    override fun onStartAnimation() {
+        val animator = ValueAnimator.ofFloat(0f, -screenHeight)
+
+        animator.addUpdateListener {
+            val value = it.animatedValue as Float
+            rocket.translationY = value
+            doge.translationY = value
+        }
+
+        animator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                Toast.makeText(applicationContext, "Doge is on the moon", Toast.LENGTH_SHORT)
+                        .show()
+                finish()
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+                Toast.makeText(applicationContext, "Doge took off", Toast.LENGTH_SHORT)
+                        .show()
+            }
+
+        })
+
+        animator.duration = 5000L
+        animator.start()
+    }
 }
