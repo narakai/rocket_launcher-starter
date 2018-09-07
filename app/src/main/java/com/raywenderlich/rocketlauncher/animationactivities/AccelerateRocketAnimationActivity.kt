879cf47a5e5e0287751de1dd095bee7b1dd8d9ec
laileon <leon@igola.com>
2018-09-07 15:18:11 +0800

    animation

--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/AccelerateRocketAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/AccelerateRocketAnimationActivity.kt
@@ -1,6 +1,20 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
+import android.animation.ValueAnimator
+import android.view.animation.AccelerateInterpolator
+
 class AccelerateRocketAnimationActivity : BaseAnimationActivity() {
-  override fun onStartAnimation() {
-  }
+    override fun onStartAnimation() {
+        val valueAnimation = ValueAnimator.ofFloat(0f, -screenHeight)
+
+        valueAnimation.addUpdateListener {
+            val value = it.animatedValue as Float
+            rocket.translationY = value
+        }
+
+        valueAnimation.interpolator = AccelerateInterpolator(1.5f)
+        valueAnimation.duration = BaseAnimationActivity.DEFAULT_ANIMATION_DURATION
+
+        valueAnimation.start()
+    }
 }
--- app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/RotateRocketAnimationActivity.kt
+++ app/src/main/java/com/raywenderlich/rocketlauncher/animationactivities/RotateRocketAnimationActivity.kt
@@ -1,6 +1,20 @@
 package com.raywenderlich.rocketlauncher.animationactivities
 
+import android.animation.ValueAnimator
+import android.view.animation.LinearInterpolator
+
 class RotateRocketAnimationActivity : BaseAnimationActivity() {
-  override fun onStartAnimation() {
-  }
+    override fun onStartAnimation() {
+        val valueAnimation = ValueAnimator.ofFloat(0f, 360f)
+
+        valueAnimation.addUpdateListener {
+            val value = it.animatedValue as Float
+            rocket.rotation = value
+        }
+
+        valueAnimation.interpolator = LinearInterpolator()
+        valueAnimation.duration = BaseAnimationActivity.DEFAULT_ANIMATION_DURATION
+
+        valueAnimation.start()
+    }
 }
